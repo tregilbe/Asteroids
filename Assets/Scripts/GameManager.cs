e0db8c3e3fe5e1213364970c6060a5b14b5490fc
Trenton <tregilbe@uat.edu>
2020-01-29 12:53:22 -0700

    Correcting repository

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score = 0;
+    public bool isPaused = false;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError(message:"I tried to create a second game manager.");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
